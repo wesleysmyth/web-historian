4fe69b50f720c52ff8b90e9aa3824d2f2651cdb8
wesleysmyth <wesleytatesmith@gmail.com>
2015-01-08 11:26:01 -0800

    Added querystrings to loading.html redirects

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,14 +1,16 @@
 var path = require('path');
 var fs = require('fs');
 var qs = require('querystring');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
 var helpers = require("./http-helpers.js");
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  var urlParts = url.parse(req.url, true, true);
   if (req.method === 'GET') {
-    if (archive.staticFiles.hasOwnProperty(req.url)) {
-      var filePath = archive.staticFiles[req.url];
+    if (archive.staticFiles.hasOwnProperty(urlParts.pathname)) {
+      var filePath = archive.staticFiles[urlParts.pathname];
       helpers.serveAssets(res, filePath, function(err, data) {
         var statusCode = 200;
         if (err) {
@@ -19,19 +21,20 @@ exports.handleRequest = function (req, res) {
         res.end();
       });
     } else {
-      // read the sites.txt file to determine if req.url exists in file
+      // read the sites.txt file to determine if urlParts.pathname exists in file
       fs.readFile(archive.paths.list, function(err, data) {
         var sites = data.toString('utf-8').split('\n');
 
         // if sites.txt contains the file
-        if (sites.indexOf(req.url.slice(1)) >= 0) {
-          var filePath = archive.paths.archivedSites + req.url;
+        if (sites.indexOf(urlParts.pathname.slice(1)) >= 0) {
+          var filePath = archive.paths.archivedSites + urlParts.pathname;
           // YES: check if site exists in the sites folder
           helpers.serveAssets(res, filePath, function(err, data) {
             if (err) {
               // NO: send a 302 to loading.html
               var statusCode = 302;
-              helpers.headers.Location = '/loading.html';
+              var queryUrl = url.format({pathname: '/loading.html', query: {site: urlParts.pathname.slice(1)}});
+              helpers.headers.Location = queryUrl;
               res.writeHead(statusCode, helpers.headers);
               delete helpers.headers.Location;
               res.end();
@@ -60,6 +63,7 @@ exports.handleRequest = function (req, res) {
         req.on('data', function(data) {
           body += data;
         });
+
         req.on('end', function() {
           siteUrl = qs.parse(body).url;
           if (sites.indexOf(siteUrl) === -1) {
@@ -70,11 +74,13 @@ exports.handleRequest = function (req, res) {
               }
             });
           }
+          var queryUrl = url.format({pathname: '/loading.html', query: {site: siteUrl}});
+          helpers.headers.Location = queryUrl;
+          res.writeHead(statusCode, helpers.headers);
+          delete helpers.headers.Location;
+          res.end();
         });
-        helpers.headers.Location = '/loading.html';
-        res.writeHead(statusCode, helpers.headers);
-        delete helpers.headers.Location;
-        res.end();
+
       });
     }
 };
